48d33af9c06c03c56082cefb7d20ae0ef45fb00f
Nagaraj <ganesannagaraj2001@gmail.com>
2025-07-12 15:07:40 +0530

    Redux toolkit setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import LoginOptionPage from "./pages/LoginOptionPage";
 import LoginRegisterPage from "./pages/LoginRegisterPage";
 import NotFound from "./pages/NotFound";
 import { useState } from "react";
+import DashBoardPage from "./pages/DashBoardPage";
 function App() {
   const [authMode, setAuthMode] = useState('login');
   const router = createBrowserRouter(
@@ -17,7 +18,8 @@ function App() {
       <Route path="/" element={<RootLayout />}>
         <Route index element={<LoginOptionPage setAuthMode={setAuthMode}/>} />
         <Route path="login" element={<LoginRegisterPage authMode={ authMode } setAuthMode={setAuthMode}/>} />
-        <Route path="register" element={<LoginRegisterPage authMode={ authMode } setAuthMode={setAuthMode}/>} />
+        <Route path="register" element={<LoginRegisterPage authMode={authMode} setAuthMode={setAuthMode} />} />
+        <Route path="dashboard" element={ <DashBoardPage /> } />
         <Route path="*" element={ <NotFound /> } />
       </Route>
     )
