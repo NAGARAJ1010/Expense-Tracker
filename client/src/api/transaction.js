3b4a84121d0c07c570bb976e3c4ceb10a2bede67
Tejas <tejas@zool.in>
2025-09-28 20:59:32 +0530

    Middleware api with default header & transaction.js with all api calls

---
+++ client/src/api/transaction.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import api from './api';
+
+export const addTransaction = async(data)=>{
+    const API_URL = `${import.meta.env.VITE_BASE_URL}/transaction/addTransaction`;
+    const result = await axios.post(API_URL, data);
+    return result;
+}
+
+export const getTransactions= async()=>{
+    const result = await api.get(`/transaction/getTransaction`);
+    return result;
+}
+
+export const getTransactionById = async(userId)=>{
+    const result = await api.get(`/transaction/getTransaction/${userId}`);
+    return result;
+}
+
+export const updateTransaction = async(transactionId)=>{
+    const result = await api.put(`/transaction/updateTransaction/${transactionId}`);
+    return result;
+}
