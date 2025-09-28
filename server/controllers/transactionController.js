2c3dc82f2e7d75d158389e7325e37c3e7687e8b3
Tejas <tejas@zool.in>
2025-09-28 15:54:40 +0530

    created transaction model, controller, router & authToken middleware to handle transaction data's

---
+++ server/controllers/transactionController.js
@@ -0,0 +1,64 @@
+const jwt = require("jsonwebtoken");
+const transactionModel = require("../models/transactionModel");
+
+exports.addTransaction = async (req, res, next) => {
+  try {
+    const transaction = new transactionModel({
+      ...req.body,
+      user: req.userId,
+    });
+
+    await transaction.save();
+    res.status(201).json({
+      success: true,
+      message: "Transaction data sent successfully...",
+    });
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+exports.getTransaction = async (res, req, next) => {
+  try {
+    const transactions = await transactionModel.find({ user: req.userId });
+    res.status(201).json({
+      success: true,
+      data: transactions,
+    });
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: "transactions not founded...",
+    });
+  }
+};
+
+exports.updateTransaction = async (req, res, next) => {
+  try {
+    const { transactionId } = req.body;
+    const existingTransaction = await transactionModel.findOneAndUpdate(
+      { _id: transactionId },
+      ...req.body,
+      { user: req.userId }
+    );
+    if (!existingTransaction) {
+      return res.status(404).json({
+        success: false,
+        message: "Transaction not found or not authorized",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: existingTransaction,
+    });
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
