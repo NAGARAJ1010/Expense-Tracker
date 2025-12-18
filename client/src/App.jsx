995d04c289ff52b92e55c4e3298adb83d9a71357
Tejas <tejas@zool.in>
2025-12-18 11:26:57 +0530

    WIP - transactions updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,16 +12,29 @@ import NotFound from "./pages/NotFound";
 import { useState } from "react";
 import DashBoardPage from "./pages/DashBoardPage";
 function App() {
-  const [authMode, setAuthMode] = useState('login');
+  const [authMode, setAuthMode] = useState("login");
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
-        <Route index element={<LoginOptionPage setAuthMode={setAuthMode}/>} />
-        <Route path="login" element={<LoginRegisterPage authMode={ authMode } setAuthMode={setAuthMode}/>} />
-        <Route path="register" element={<LoginRegisterPage authMode={authMode} setAuthMode={setAuthMode} />} />
-        <Route path="register/success" element={<LoginRegisterPage success={true} />} />
-        <Route path="dashboard" element={ <DashBoardPage /> } />
-        <Route path="*" element={ <NotFound /> } />
+        <Route index element={<LoginOptionPage setAuthMode={setAuthMode} />} />
+        <Route
+          path="login"
+          element={
+            <LoginRegisterPage authMode={"login"} setAuthMode={setAuthMode} />
+          }
+        />
+        <Route
+          path="register"
+          element={
+            <LoginRegisterPage authMode={authMode} setAuthMode={setAuthMode} />
+          }
+        />
+        <Route
+          path="register/success"
+          element={<LoginRegisterPage success={true} />}
+        />
+        <Route path="dashboard" element={<DashBoardPage />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     )
   );
--- server/app.js
+++ server/app.js
@@ -9,17 +9,17 @@ const app = express();
 
 const authRouter = require('./routes/authRouter');
 const transactionRouter = require('./routes/transactionRouter');
-dotenv.config({path: path.join(__dirname,'config','config.env')});
+dotenv.config({ path: path.join(__dirname, 'config', 'config.env') });
 app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api/auth',authRouter);
-app.use('/api/transaction',transactionRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/transaction', transactionRouter);
 
-mongoose.connect(process.env.DB_URL).then(()=>console.log('DB connected...')).catch(err => console.log(err));
-app.listen(process.env.PORT,()=>{
-    console.log(process.env.NODE_ENV,'server is running at ',process.env.PORT,' port...');
+mongoose.connect(process.env.DB_URL).then(() => console.log(process.env.DB_URL + 'DB connected...')).catch(err => console.log(err));
+app.listen(process.env.PORT, () => {
+    console.log(process.env.NODE_ENV, 'server is running at ', process.env.PORT, ' port...');
 });
